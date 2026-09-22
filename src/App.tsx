27b9766bbc81003ca2b82646a4d16263b51c1330
claydotd <claydleslie@icloud.com>
2026-09-22 17:36:00 +0100

    Page Titles fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-
 import { useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
 import { Outlet, useLocation } from 'react-router-dom'
+import { galleries } from 'virtual:galleries'
+import { getContentPage } from './contentPages'
+
+const SITE_TITLE = 'Grounded Presence'
+
+function titleForPath(pathname: string): string {
+  const segments = pathname.split('/').filter(Boolean)
+
+  if (segments.length === 0) {
+    return SITE_TITLE
+  }
+
+  if (segments[0] === 'work') {
+    if (!segments[1]) return `Work | ${SITE_TITLE}`
+
+    const gallery = galleries.find((item) => item.slug === segments[1])
+    return gallery ? `${gallery.name} | ${SITE_TITLE}` : `Work | ${SITE_TITLE}`
+  }
+
+  const page = getContentPage(segments[0])
+  return page ? `${page.title} | ${SITE_TITLE}` : SITE_TITLE
+}
 
 function ScrollToTop() {
   const { pathname } = useLocation()
@@ -16,12 +37,23 @@ function ScrollToTop() {
   return null
 }
 
+function DocumentTitle() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    document.title = titleForPath(pathname)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   const { pathname } = useLocation()
 
   return (
     <>
       <ScrollToTop />
+      <DocumentTitle />
       <Header />
       <main key={pathname} className="page">
         <Outlet />
